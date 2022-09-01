062266b97741fbca0effc678637b3b3552c3b18c
luanleecf <luanleedacostafaria@gmail.com>
2022-08-31 23:07:26 -0300

    @input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,21 @@ import { Component, OnInit } from '@angular/core';
   template: `
   <!-- <app-data-biding></app-data-biding> -->
   <!-- <app-diretivas-estruturais></app-diretivas-estruturais> -->
-  <app-new-component></app-new-component>
-  <app-diretivas-atributos></app-diretivas-atributos>
+  <!-- <app-diretivas-atributos></app-diretivas-atributos> -->
+  <!-- <app-new-component></app-new-component> -->
+  <app-input [contador]="addValue"></app-input>
+  <br>
+  <button (click)="add()">Adiciona</button>
   <router-outlet></router-outlet>`,
 })
 export class AppComponent implements OnInit {
 
+  public addValue: number = 0;
+
   constructor() { }
   ngOnInit(): void { }
+
+  public add() {
+    this.addValue += 1;
+  }
 }
